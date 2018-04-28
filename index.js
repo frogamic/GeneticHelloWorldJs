5b27952264afa9fb942e8115d0e100eb2e2d5aef
Dominic Shelton <frogamic@gmail.com>
2018-04-28 21:52:48 +1200

    Add early exit condition, improve score summing

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 'use strict';
 const MUTATE_RATE = 0.01;
 const BREED_RATE = 0.75;
-const POPULATION_SIZE = 100;
-const TARGET = 'Hello World';
+const POPULATION_SIZE = 1000;
+const TARGET = 'Hello, World';
 
 const generateCharacter = () => {
     return String.fromCharCode(Math.floor(Math.random() * 94) + 32);
@@ -64,15 +64,20 @@ console.log(`Using a population size of ${POPULATION_SIZE},`);
 console.log(`Regenerating ${BREED_RATE * 100}% of the population per generation,`);
 console.log(`${MUTATE_RATE * 100}% chance of mutation for each chromosome.`);
 
-while (population[0] !== TARGET) {
+do {
     generation += 1;
 
     let results = population.map(checkFitness).sort((x, y) => {return y.score - x.score;});
-    console.log(`Generation ${generation}: '${results[0].value}', score: ${results[0].score}`);
-    let elders = results.slice(0, POPULATION_SIZE * (1 - BREED_RATE));
-    population = elders.map(x => x.value);
-    for (let i = 0; i < POPULATION_SIZE * BREED_RATE; ++i) {
+    if (results[0].value !== TARGET) {
+        let elders = results.slice(0, POPULATION_SIZE * (1 - BREED_RATE));
+        population = elders.map(x => x.value);
         let totalScore = elders.reduce((a, x) => {return a + x.score;}, 0);
-        population.push(breed(selectParent(elders, totalScore).value, selectParent(elders, totalScore).value));
+        for (let i = 0; i < POPULATION_SIZE * BREED_RATE; ++i) {
+            population.push(breed(selectParent(elders, totalScore).value, selectParent(elders, totalScore).value));
+        }
+    } else {
+        population = results.map(x => x.value);
     }
-}
+
+    console.log(`Generation ${generation}: '${results[0].value}', score: ${results[0].score}`);
+} while (population[0] !== TARGET)
